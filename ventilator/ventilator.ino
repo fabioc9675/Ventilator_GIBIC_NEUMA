8e19449a6129e1765a0e1a580ea9e1ccbe32254c
fabioc9675 <fabian.castano@yahoo.es>
2020-03-31 16:31:46 -0500

    Stable version 31-03-2020

--- ventilator/ventilator.ino
+++ ventilator/ventilator.ino
@@ -58,9 +58,9 @@ String RaspberryChain = "";
 
 // estructura para el conteo de los pulsos
 struct Fan {
-	const uint8_t PIN;
-	uint32_t numberOfPulses;
-	bool aspa;
+  const uint8_t PIN;
+  uint32_t numberOfPulses;
+  bool aspa;
 };
 
 Fan fan1 = { FLANCO, 0, false };
@@ -95,8 +95,8 @@ int milisecond = 0;
 // *********************************
 
 // definiciones del ADC
-float Pin[10] = { 0,0,0,0,0,0,0,0,0,0 };
-float Pout[10] = { 0,0,0,0,0,0,0,0,0,0 };
+float Pin[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+float Pout[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 float SPin = 0;
 float SPout = 0;
 float Peep = 0;
@@ -115,9 +115,9 @@ float C1 = 1.3836;
 float C2 = 38.2549;
 float C3 = 291.7069;
 float VT = 0;
-float V_Comp[10] = { 0,0,0,0,0,0,0,0,0,0 };
-float V_dpmax[10] = { 0,0,0,0,0,0,0,0,0,0 };
-float V_dpmin[10] = { 0,0,0,0,0,0,0,0,0,0 };
+float V_Comp[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+float V_dpmax[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+float V_dpmin[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 float SComp = 0;
 float Sdpmax = 0;
 float Sdpmin = 0;
@@ -144,54 +144,54 @@ void IRAM_ATTR encoderInterrupt_B();
 
 
 void setup() {
-	// Configuracion del timer a 1 kHz
-	timer = timerBegin(0, 80, true);               // Frecuencia de reloj 80 MHz, prescaler de 80, frec 1 MHz
-	timerAttachInterrupt(timer, &onTimer, true);
-	timerAlarmWrite(timer, 1000, true);             // Interrupcion cada 1000 conteos del timer, es decir 100 Hz
-	timerAlarmEnable(timer);                        // Habilita interrupcion por timer
+  // Configuracion del timer a 1 kHz
+  timer = timerBegin(0, 80, true);               // Frecuencia de reloj 80 MHz, prescaler de 80, frec 1 MHz
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 1000, true);             // Interrupcion cada 1000 conteos del timer, es decir 100 Hz
+  timerAlarmEnable(timer);                        // Habilita interrupcion por timer
 
 
-	//Encoder setup
-	pinMode(A, INPUT_PULLUP);    // A como entrada
-	pinMode(B, INPUT_PULLUP);    // B como entrada
-	pinMode(SW, INPUT_PULLUP);   // SW como entrada
+  //Encoder setup
+  pinMode(A, INPUT_PULLUP);    // A como entrada
+  pinMode(B, INPUT_PULLUP);    // B como entrada
+  pinMode(SW, INPUT_PULLUP);   // SW como entrada
 
-	attachInterrupt(digitalPinToInterrupt(A), encoderInterrupt_A, FALLING);
-	attachInterrupt(digitalPinToInterrupt(B), encoderInterrupt_B, FALLING);
-	attachInterrupt(digitalPinToInterrupt(SW), swInterrupt, RISING);
+  attachInterrupt(digitalPinToInterrupt(A), encoderInterrupt_A, FALLING);
+  attachInterrupt(digitalPinToInterrupt(B), encoderInterrupt_B, FALLING);
+  attachInterrupt(digitalPinToInterrupt(SW), swInterrupt, RISING);
 
-	// configuracion del pin de interrupcion
-	pinMode(fan1.PIN, INPUT_PULLUP);
-	//attachInterrupt(fan1.PIN, isr, RISING);
+  // configuracion del pin de interrupcion
+  pinMode(fan1.PIN, INPUT_PULLUP);
+  //attachInterrupt(fan1.PIN, isr, RISING);
 
-	// inicializacion de los pines controladores de las EV como salidas
-	pinMode(EV_01_P1, OUTPUT);  // PIN 3   velocidad
-	//  pinMode(EV_01_P2, OUTPUT);  // PIN 4   direccion
-	pinMode(EV_02_P1, OUTPUT);  // PIN 6   velocidad
-	//  pinMode(EV_02_P2, OUTPUT);  // PIN 7   direccion
-	pinMode(EV_03_P1, OUTPUT);  // PIN 12  velocidad
-	//  pinMode(EV_03_P2, OUTPUT);  // PIN 11  direccion
+  // inicializacion de los pines controladores de las EV como salidas
+  pinMode(EV_01_P1, OUTPUT);  // PIN 3   velocidad
+  //  pinMode(EV_01_P2, OUTPUT);  // PIN 4   direccion
+  pinMode(EV_02_P1, OUTPUT);  // PIN 6   velocidad
+  //  pinMode(EV_02_P2, OUTPUT);  // PIN 7   direccion
+  pinMode(EV_03_P1, OUTPUT);  // PIN 12  velocidad
+  //  pinMode(EV_03_P2, OUTPUT);  // PIN 11  direccion
 
-	Serial.begin(115200);   // incializacion de comunicacion serie a 9600 bps
-	Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
-	lcd_setup();
+  Serial.begin(115200);   // incializacion de comunicacion serie a 9600 bps
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
+  lcd_setup();
 
-	// inicializacion de los strings
-	SERIALEQU = String("1234567890ABCD");
-	patientPress = String("");
-	circuitPress = String("");
-	patientFlow = String("234.67");
-	circuitFlow = String("245.67");
-	inspTime = String("2.1");
-	expiTime = String("3.5");
-	frequency = String("20");
-	volume = String("240");
-	RaspberryChain = String("");
+  // inicializacion de los strings
+  SERIALEQU = String("1234567890ABCD");
+  patientPress = String("");
+  circuitPress = String("");
+  patientFlow = String("234.67");
+  circuitFlow = String("245.67");
+  inspTime = String("2.1");
+  expiTime = String("3.5");
+  frequency = String("20");
+  volume = String("240");
+  RaspberryChain = String("");
 
-	lcd_show();
+  lcd_show();
 
 
-pinMode(14, OUTPUT);
+  pinMode(14, OUTPUT);
 
 }
 
@@ -219,11 +219,11 @@ void loop() {
       //  digitalWrite(EV_02_P2, LOW);   // turn the LED on (HIGH is the voltage level)
       digitalWrite(EV_03_P1, LOW);   // turn the LED on (HIGH is the voltage level)
       //  digitalWrite(EV_03_P2, LOW);   // turn the LED on (HIGH is the voltage level)
-      
+
       //Calculos
-			Pin_min = SPin;
-			Pout_min = SPout;
-      
+      Pin_min = SPin;
+      Pout_min = SPout;
+
     }
     else if (interruptCounter == int(inspirationTime * 1000)) { // espera 1 segundo y cierra electrovalvula de entrada y abre electrovalvula de salida
       //  digitalWrite(EV_01_P2, LOW);    // turn the LED off by making the voltage LOW
@@ -233,54 +233,58 @@ void loop() {
       //  digitalWrite(EV_02_P2, HIGH);   // turn the LED on (HIGH is the voltage level)
       digitalWrite(EV_03_P1, HIGH);    // turn the LED off by making the voltage LOW
       //  digitalWrite(EV_03_P2, HIGH);   // turn the LED on (HIGH is the voltage level)
-      
+
       //Calculos
-			Pin_max = SPin;
-			Pout_max = SPout;
-			dpmax = Pin_max - Pin_min;
-			if (dpmax < 0) {
-				dpmax = -dpmax;
-			}
-			dpmin = Pout_max - Pout_min;
-			if (dpmin < 0) {
-				dpmin = -dpmin;
-			}
-      
+      Pin_max = SPin;
+      Pout_max = SPout;
+      dpmax = Pin_max - Pin_min;
+      if (dpmax < 0) {
+        dpmax = -dpmax;
+      }
+      dpmin = Pout_max - Pout_min;
+      if (dpmin < 0) {
+        dpmin = -dpmin;
+      }
+
     }
     else if (interruptCounter >= int(((inspirationTime + expirationTime) * 1000))) {
       interruptCounter = 0;
       Peep = UmbralPeep;
-			UmbralPeep = 100;
-			Ppico = UmbralPpico - 2;
-			UmbralPpico = -100;
-			Comp = (C1 * Peep * Peep) - (C2 * Peep) + C3;
-			//Comp=(-14.52*Peep)+(198.75);
+      UmbralPeep = 100;
+      Ppico = UmbralPpico - 2;
+      UmbralPpico = -100;
+      Comp = (C1 * Peep * Peep) - (C2 * Peep) + C3;
+      //Comp=(-14.52*Peep)+(198.75);
 
-			//Filtrado media movil VT y COMP
-			//-Almacenamiento de valores
-			V_Comp[9] = Comp;
-			V_dpmax[9] = dpmax;
-			V_dpmin[9] = dpmin;
-			//-Corrimiento inicial
-			for (char i = 9; i >= 1; i--) {
-				V_Comp[9 - i] = V_Comp[9 - i + 1];
-				V_dpmax[9 - i] = V_dpmax[9 - i + 1];
-				V_dpmin[9 - i] = V_dpmin[9 - i + 1];
-			}
-			//-Calculo promedio
-			SComp = 0;
-			Sdpmax = 0;
-			Sdpmin = 0;
-			for (char i = 0; i <= 9; i++) {
-				SComp = SComp + V_Comp[i];
-				Sdpmax = Sdpmax + V_dpmax[i];
-				Sdpmin = Sdpmin + V_dpmin[i];
-			}
-			SComp = SComp / 10;
-			Sdpmax = Sdpmax / 10;
-			Sdpmin = Sdpmin / 10;
-			VT = SComp * (Sdpmax - Sdpmin);
-	
+      //Filtrado media movil VT y COMP
+      //-Almacenamiento de valores
+      V_Comp[9] = Comp;
+      V_dpmax[9] = dpmax;
+      V_dpmin[9] = dpmin;
+      //-Corrimiento inicial
+      for (char i = 9; i >= 1; i--) {
+        V_Comp[9 - i] = V_Comp[9 - i + 1];
+        V_dpmax[9 - i] = V_dpmax[9 - i + 1];
+        V_dpmin[9 - i] = V_dpmin[9 - i + 1];
+      }
+      //-Calculo promedio
+      SComp = 0;
+      Sdpmax = 0;
+      Sdpmin = 0;
+      for (char i = 0; i <= 9; i++) {
+        SComp = SComp + V_Comp[i];
+        Sdpmax = Sdpmax + V_dpmax[i];
+        Sdpmin = Sdpmin + V_dpmin[i];
+      }
+      SComp = SComp / 10;
+      Sdpmax = Sdpmax / 10;
+      Sdpmin = Sdpmin / 10;
+      VT = SComp * (Sdpmax - Sdpmin);
+
+      digitalWrite(14, HIGH);
+      lcd_show();
+      digitalWrite(14, LOW);
+
     }
 
 
@@ -290,7 +294,7 @@ void loop() {
         flagDetach = true;
       }
       contDetach++;
-      if (contDetach >= 150) {
+      if (contDetach >= 100) {
         attachInterrupt(digitalPinToInterrupt(A), encoderInterrupt_A, FALLING);
         flagDettachInterrupt_A = false;
         flagDetach = false;
@@ -307,7 +311,7 @@ void loop() {
         flagDetach = true;
       }
       contDetach++;
-      if (contDetach >= 150) {
+      if (contDetach >= 100) {
         attachInterrupt(digitalPinToInterrupt(B), encoderInterrupt_B, FALLING);
         flagDettachInterrupt_B = false;
         flagDetach = false;
@@ -317,7 +321,7 @@ void loop() {
         digitalWrite(14, LOW);
       }
     }
-    
+
     milisecond ++;
     if (milisecond == 1000) {
       milisecond = 0;
@@ -335,73 +339,73 @@ void loop() {
   // *************************************************
   if (fl_ADC) {
     fl_ADC = false;
-		ADC1_Value = analogRead(ADC_PRESS_1);
-		ADC2_Value = analogRead(ADC_PRESS_2);
-		ADC3_Value = analogRead(ADC_PRESS_3);
-		// falta realizatr la conversion de adc a presion
-		Pressure1 = AMP1 * float(ADC1_Value) + OFFS1;
-		Pressure2 = AMP2 * float(ADC2_Value) + OFFS2;
-		Pressure3 = AMP3 * float(ADC3_Value) + OFFS3;
+    ADC1_Value = analogRead(ADC_PRESS_1);
+    ADC2_Value = analogRead(ADC_PRESS_2);
+    ADC3_Value = analogRead(ADC_PRESS_3);
+    // falta realizatr la conversion de adc a presion
+    Pressure1 = AMP1 * float(ADC1_Value) + OFFS1;
+    Pressure2 = AMP2 * float(ADC2_Value) + OFFS2;
+    Pressure3 = AMP3 * float(ADC3_Value) + OFFS3;
 
-		//Filtrado media movil
-	//-Almacenamiento de valores
-		Pin[9] = Pressure1;
-		Pout[9] = Pressure2;
-		//-Corrimiento inicial
-		for (char i = 9; i >= 1; i--) {
-			Pin[9 - i] = Pin[9 - i + 1];
-			Pout[9 - i] = Pout[9 - i + 1];
-		}
-		//-Calculo promedio
-		SPin = 0;
-		SPout = 0;
-		for (char i = 0; i <= 9; i++) {
-			SPin = SPin + Pin[i];
-			SPout = SPout + Pout[i];
-		}
-		SPin = SPin / 10;
-		SPout = SPout / 10;
-		//-Calculo Peep
-		if (UmbralPeep > SPin) {
-			UmbralPeep = SPin;
-		}
-		//-Calculo Ppico
-		if (UmbralPpico < SPin) {
-			UmbralPpico = SPin;
-		}
+    //Filtrado media movil
+    //-Almacenamiento de valores
+    Pin[9] = Pressure1;
+    Pout[9] = Pressure2;
+    //-Corrimiento inicial
+    for (char i = 9; i >= 1; i--) {
+      Pin[9 - i] = Pin[9 - i + 1];
+      Pout[9 - i] = Pout[9 - i + 1];
+    }
+    //-Calculo promedio
+    SPin = 0;
+    SPout = 0;
+    for (char i = 0; i <= 9; i++) {
+      SPin = SPin + Pin[i];
+      SPout = SPout + Pout[i];
+    }
+    SPin = SPin / 10;
+    SPout = SPout / 10;
+    //-Calculo Peep
+    if (UmbralPeep > SPin) {
+      UmbralPeep = SPin;
+    }
+    //-Calculo Ppico
+    if (UmbralPpico < SPin) {
+      UmbralPpico = SPin;
+    }
 
-		// conteo de los pulsos
-		numeroPulsos = fan1.numberOfPulses;  //Numero de pulsos en 50ms
-		fan1.numberOfPulses = 0;
-		numeroPulsos = (numeroPulsos * 2000 / 75); //(#pulsos/1 s)=7.5*Q [L/min] (considera un decimal)
+    // conteo de los pulsos
+    numeroPulsos = fan1.numberOfPulses;  //Numero de pulsos en 50ms
+    fan1.numberOfPulses = 0;
+    numeroPulsos = (numeroPulsos * 2000 / 75); //(#pulsos/1 s)=7.5*Q [L/min] (considera un decimal)
 
-		patientPress = String(Pressure1);
-		circuitPress = String(Pressure2);
+    patientPress = String(Pressure1);
+    circuitPress = String(Pressure2);
 
 
-		if (pruebaDato == 20) {
-			pruebaDato = 0;
-		}
-		frequency = String(pruebaDato);
-		volume = String(second);
-		expiTime = String(inspirationTime * 1000);
-		inspTime = String(expirationTime * 1000);
+    if (pruebaDato == 20) {
+      pruebaDato = 0;
+    }
+    frequency = String(pruebaDato);
+    volume = String(second);
+    expiTime = String(inspirationTime * 1000);
+    inspTime = String(expirationTime * 1000);
 
-		pruebaDato++;
+    pruebaDato++;
 
-		// composicion de cadena
-		RaspberryChain = SERIALEQU + ',' + patientPress + ',' + circuitPress + ',' + patientFlow + ',' + circuitFlow + ',' + inspTime + ',' + expiTime + ',' + frequency + ',' + volume;
+    // composicion de cadena
+    RaspberryChain = SERIALEQU + ',' + patientPress + ',' + circuitPress + ',' + patientFlow + ',' + circuitFlow + ',' + inspTime + ',' + expiTime + ',' + frequency + ',' + volume;
 
-		// Envio de la cadena de datos
-		//Serial.println(RaspberryChain);
-		//Serial2.println(RaspberryChain);
+    // Envio de la cadena de datos
+    //Serial.println(RaspberryChain);
+    Serial2.println(RaspberryChain);
 
-		/*Serial.print("Pres1 = ");
-		  Serial.print(ADC1_Value);
-		  Serial.print(", Pres2 = ");
-		  Serial.print(ADC2_Value + 180);
-		  Serial.print(", Puls = ");
-		  Serial.println(numeroPulsos);*/
+    /*Serial.print("Pres1 = ");
+      Serial.print(ADC1_Value);
+      Serial.print(", Pres2 = ");
+      Serial.print(ADC2_Value + 180);
+      Serial.print(", Puls = ");
+      Serial.println(numeroPulsos);*/
   }
   // Final muestreo ADC
 
@@ -437,12 +441,12 @@ void loop() {
     encoderInterruptAttention_B();
   }
   // Final interrupcion encoder
-	         
+
 }
 
 void IRAM_ATTR onTimer() {
-	portENTER_CRITICAL(&timerMux);
-	//Serial.println("I am inside onTimer");
-	flagTimerInterrupt = true;
-	portEXIT_CRITICAL(&timerMux);
+  portENTER_CRITICAL(&timerMux);
+  //Serial.println("I am inside onTimer");
+  flagTimerInterrupt = true;
+  portEXIT_CRITICAL(&timerMux);
 }

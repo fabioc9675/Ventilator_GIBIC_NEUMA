70497b64a3a8fd998dcd31471cd640244d6b74c6
helbercarvajal <handres.carvajal@gmail.com>
2020-04-03 11:44:00 -0500

    Add LCD control project for one of the ESP32

---
+++ lcd_control/lcd_control.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		lcd_control.ino
+ Created:	4/3/2020 11:40:24
+ Author:	Aleph
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}

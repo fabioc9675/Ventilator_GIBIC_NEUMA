bfa5b21b09c67561fce9f213017d0067e6964e55
helbercarvajal <handres.carvajal@gmail.com>
2020-04-03 10:29:00 -0500

    Stable version. It is necesary to be careful with the LCD delay, because affect the Cycling of the ventilator.

--- ventilator/utilities.ino
+++ ventilator/utilities.ino
@@ -20,232 +20,352 @@ unsigned int debounceVal = 1;  // valor para el antirrebote
 
 // Interrupcion por presion del switch
 void IRAM_ATTR swInterrupt() {
-  portENTER_CRITICAL_ISR(&mux);
-  flagSwInterrupt = true;
-  tiempoInterrupcion = millis();
-  portEXIT_CRITICAL_ISR(&mux);
+	portENTER_CRITICAL_ISR(&mux);
+	flagSwInterrupt = true;
+	tiempoInterrupcion = millis();
+	portEXIT_CRITICAL_ISR(&mux);
 }
 
 
 // Interrupcion por encoder
 void IRAM_ATTR encoderInterrupt_A() {
-  portENTER_CRITICAL_ISR(&mux);
-  flagEncoderInterrupt_A = true;
-  tiempoDebounce_A = millis();
-  portEXIT_CRITICAL_ISR(&mux);
+	portENTER_CRITICAL_ISR(&mux);
+	flagEncoderInterrupt_A = true;
+	tiempoDebounce_A = millis();
+	portEXIT_CRITICAL_ISR(&mux);
 }
 
 
 // Interrupcion por encoder
 void IRAM_ATTR encoderInterrupt_B() {
-  portENTER_CRITICAL_ISR(&mux);
-  flagEncoderInterrupt_B = true;
-  tiempoDebounce_B = millis();
-  portEXIT_CRITICAL_ISR(&mux);
+	portENTER_CRITICAL_ISR(&mux);
+	flagEncoderInterrupt_B = true;
+	tiempoDebounce_B = millis();
+	portEXIT_CRITICAL_ISR(&mux);
 }
 
 void swInterruptAttention() {
 
-  if (tiempoInterrupcion - ultimaInterrupcion > 100) {
-    // portENTER_CRITICAL_ISR(&mux);
-    switchRoutine();
-    // Serial.println(tiempoInterrupcion - ultimaInterrupcion);
-    // portEXIT_CRITICAL_ISR(&mux);
-    ultimaInterrupcion = tiempoInterrupcion;
-  }
+	if (tiempoInterrupcion - ultimaInterrupcion > 100) {
+		// portENTER_CRITICAL_ISR(&mux);
+		switchRoutine();
+		// Serial.println(tiempoInterrupcion - ultimaInterrupcion);
+		// portEXIT_CRITICAL_ISR(&mux);
+		ultimaInterrupcion = tiempoInterrupcion;
+	}
 }
 
 void encoderInterruptAttention_A() {
 
-  if (tiempoDebounce_A - debounceUltima_A > debounceVal) {
-    // Serial.print("A = ");
-    // Serial.println(tiempoDebounce_A - debounceUltima_A);
-    if ((digitalRead(B) == HIGH) && (digitalRead(A) == LOW)) {
-      // portENTER_CRITICAL_ISR(&mux);
-      flagEncoder = 0;
-      flagDettachInterrupt_B = true;
-      contDetach = 0;
-      encoderRoutine();
-      // portEXIT_CRITICAL_ISR(&mux);
-    }
-    debounceUltima_A = tiempoDebounce_A;
-  }
+	if (tiempoDebounce_A - debounceUltima_A > debounceVal) {
+		// Serial.print("A = ");
+		// Serial.println(tiempoDebounce_A - debounceUltima_A);
+		if ((digitalRead(B) == HIGH) && (digitalRead(A) == LOW)) {
+			// portENTER_CRITICAL_ISR(&mux);
+			flagEncoder = 0;
+			flagDettachInterrupt_B = true;
+			contDetach = 0;
+			encoderRoutine();
+			// portEXIT_CRITICAL_ISR(&mux);
+		}
+		debounceUltima_A = tiempoDebounce_A;
+	}
 }
 
 void encoderInterruptAttention_B() {
 
-  if (tiempoDebounce_B - debounceUltima_B > debounceVal) {
-    // Serial.print("B = ");
-    // Serial.println(tiempoDebounce_B - debounceUltima_B);
-    if ((digitalRead(A) == HIGH) && (digitalRead(B) == LOW)) {
-      // portENTER_CRITICAL_ISR(&mux);
-      flagEncoder = 1;
-      flagDettachInterrupt_A = true;
-      contDetach = 0;
-      encoderRoutine();
-      // portEXIT_CRITICAL_ISR(&mux);
-    }
-    debounceUltima_B = tiempoDebounce_B;
-  }
+	if (tiempoDebounce_B - debounceUltima_B > debounceVal) {
+		// Serial.print("B = ");
+		// Serial.println(tiempoDebounce_B - debounceUltima_B);
+		if ((digitalRead(A) == HIGH) && (digitalRead(B) == LOW)) {
+			// portENTER_CRITICAL_ISR(&mux);
+			flagEncoder = 1;
+			flagDettachInterrupt_A = true;
+			contDetach = 0;
+			encoderRoutine();
+			// portEXIT_CRITICAL_ISR(&mux);
+		}
+		debounceUltima_B = tiempoDebounce_B;
+	}
 }
 
 
 void switchRoutine() {
 
-  // Serial.println("SW MENU");
-  if (menu == 1 && !insideMenuFlag) {
-    insideMenuFlag = !insideMenuFlag;
-    flagFrecuencia = true;
-    // Serial.println("SW MENU 1_1");
-  }
-  else if (menu == 1 && flagFrecuencia) {
-    flagFrecuencia = false;
-    flagIE = true;
-    // Serial.println("SW MENU 1_2");
-  }
-  else if (menu == 1 && flagIE) {
-    insideMenuFlag = !insideMenuFlag;
-    flagIE = false;
-    // Serial.println("SW MENU 1_3");
-  }
-  else if (menu == 2 && !insideMenuFlag) {
-    insideMenuFlag = !insideMenuFlag;
-    flagPresion = true;
-    Serial.println("Config maxPres");
-  }
-  else if (menu == 2 && flagPresion) {
-    flagPresion = false;
-    flagFlujo = true;
-    Serial.println("Config maxFlujo");
-  }
-  else if (menu == 2 && flagFlujo) {
-    insideMenuFlag = !insideMenuFlag;
-    flagFlujo = false;
-  }
+	// Serial.println("SW MENU");
+	if (menu == 1 && !insideMenuFlag) {
+		insideMenuFlag = !insideMenuFlag;
+		flagFrecuencia = true;
+		// Serial.println("SW MENU 1_1");
+	}
+	else if (menu == 1 && flagFrecuencia) {
+		flagFrecuencia = false;
+		flagIE = true;
+		// Serial.println("SW MENU 1_2");
+	}
+	else if (menu == 1 && flagIE) {
+		insideMenuFlag = !insideMenuFlag;
+		flagIE = false;
+		// Serial.println("SW MENU 1_3");
+	}
+	else if (menu == 2 && !insideMenuFlag) {
+		insideMenuFlag = !insideMenuFlag;
+		flagPresion = true;
+		Serial.println("Config maxPres");
+	}
+	else if (menu == 2 && flagPresion) {
+		flagPresion = false;
+		flagFlujo = true;
+		Serial.println("Config maxFlujo");
+	}
+	else if (menu == 2 && flagFlujo) {
+		insideMenuFlag = !insideMenuFlag;
+		flagFlujo = false;
+	}
 
-  lcd_show();
+	lcd_show();
 
 }
 
 void encoderRoutine() {
-  static int cont = 0;
-  // Incremento
-  if (flagEncoder == 1) {
-    if (insideMenuFlag == false) {
-      menu++;
-      if (menu < 0 || menu > MENU_QUANTITY - 1)
-        menu = 0;
-      //Serial.println("menu = " + String(menu));
-    }
-    else {
-      switch (menu) {
-        case 1:
-          if (flagFrecuencia) {
-            frecRespiratoria++;
-            if (frecRespiratoria > MAX_FREC) {
-              frecRespiratoria = MAX_FREC;
-            }
-          }
-          else if (flagIE) {
+	static int cont = 0;
+	// Incremento
+	if (flagEncoder == 1) {
+		if (insideMenuFlag == false) {
+			menu++;
+			if (menu < 0 || menu > MENU_QUANTITY - 1)
+				menu = 0;
+			//Serial.println("menu = " + String(menu));
+		}
+		else {
+			switch (menu) {
+			case 1:
+				if (flagFrecuencia) {
+					frecRespiratoria++;
+					if (frecRespiratoria > MAX_FREC) {
+						frecRespiratoria = MAX_FREC;
+					}
+				}
+				else if (flagIE) {
 
-            relacionIE = relacionIE + 0.1;
-            if (relacionIE >= MAX_RIE) {
-              relacionIE = MAX_RIE;;
-            }
-            if (relacionIE > -1.0 && relacionIE < 0) {
-              relacionIE = 1;
+					relacionIE = relacionIE + 0.1;
+					if (relacionIE >= MAX_RIE) {
+						relacionIE = MAX_RIE;;
+					}
+					if (relacionIE > -1.0 && relacionIE < 0) {
+						relacionIE = 1;
 
-            }
-            // Calculo del tiempo I:E
-            if (relacionIE > 0) {
-              inspirationTime = (60 / frecRespiratoria) / (1 + relacionIE);
-              expirationTime = relacionIE * inspirationTime;
-            }
-            else {
-              expirationTime = (60 / frecRespiratoria) / (1 - relacionIE);
-              inspirationTime = -relacionIE * expirationTime;
-            }
-          }
-          //Serial.println("I :" + String(inspirationTime));
-          //Serial.println("E :" + String(expirationTime));
-          break;
-        case 2:
-          if (flagPresion) {
+					}
+					// Calculo del tiempo I:E
+					if (relacionIE > 0) {
+						inspirationTime = (60 / frecRespiratoria) / (1 + relacionIE);
+						expirationTime = relacionIE * inspirationTime;
+					}
+					else {
+						expirationTime = (60 / frecRespiratoria) / (1 - relacionIE);
+						inspirationTime = -relacionIE * expirationTime;
+					}
+				}
+				//Serial.println("I :" + String(inspirationTime));
+				//Serial.println("E :" + String(expirationTime));
+				break;
+			case 2:
+				if (flagPresion) {
 
-            maxPresion++;
-            if (maxPresion > MAX_PRESION) {
-              maxPresion = MAX_PRESION;
-            }
-          }
-          else if (flagFlujo) {
+					maxPresion++;
+					if (maxPresion > MAX_PRESION) {
+						maxPresion = MAX_PRESION;
+					}
+				}
+				else if (flagFlujo) {
 
-            maxFlujo++;
-            if (maxFlujo > MAX_FLUJO) {
-              maxFlujo = MAX_FLUJO;
-            }
-          }
-          break;
-      }
-    }
-  }
-  // Decremento
-  else if (flagEncoder == 0) {
-    if (insideMenuFlag == false) {
-      menu--;
-      if (menu < 0 || menu > MENU_QUANTITY - 1)
-        menu = MENU_QUANTITY - 1;
-      //Serial.println("menu = " + String(menu));
-    }
-    else {
-      switch (menu) {
-        case 1:
-          if (flagFrecuencia) {
-              frecRespiratoria--;
-              if (frecRespiratoria > MAX_FREC) {
-                frecRespiratoria = 0;
-              }
-          }
-          else if (flagIE) {
-              relacionIE = relacionIE - 0.1;
-              if (relacionIE <= -MAX_RIE) {
-                relacionIE = -MAX_RIE;
-              }
-              if (relacionIE > 0 && relacionIE < 1) {
-                relacionIE = -1;
-              }
-            // Calculo del tiempo I:E
-            if (relacionIE > 0) {
-              inspirationTime = (60 / frecRespiratoria) / (1 + relacionIE);
-              expirationTime = relacionIE * inspirationTime;
-            }
-            else {
-              expirationTime = (60 / frecRespiratoria) / (1 - relacionIE);
-              inspirationTime = -relacionIE * expirationTime;
-            }
-          }
-          //Serial.println("I :" + String(inspirationTime));
-          //Serial.println("E :" + String(expirationTime));
-          break;
-        case 2:
-          if (flagPresion) {
-              maxPresion--;
-              if (maxPresion > MAX_PRESION) {
-                maxPresion = MAX_PRESION;
-              }
-          }
-          else if (flagFlujo) {
-              maxFlujo--;
-              if (maxFlujo > MAX_FLUJO) {
-                maxFlujo = MAX_FLUJO;
-              }
-          }
-          break;
-      }
-    }
-  }
-  // portENTER_CRITICAL_ISR(&mux);
-  flagEncoder = 3;
-  //lcd_show();
+					maxFlujo++;
+					if (maxFlujo > MAX_FLUJO) {
+						maxFlujo = MAX_FLUJO;
+					}
+				}
+				break;
+			}
+		}
+	}
+	// Decremento
+	else if (flagEncoder == 0) {
+		if (insideMenuFlag == false) {
+			menu--;
+			if (menu < 0 || menu > MENU_QUANTITY - 1)
+				menu = MENU_QUANTITY - 1;
+			//Serial.println("menu = " + String(menu));
+		}
+		else {
+			switch (menu) {
+			case 1:
+				if (flagFrecuencia) {
+					frecRespiratoria--;
+					if (frecRespiratoria > MAX_FREC) {
+						frecRespiratoria = 0;
+					}
+				}
+				else if (flagIE) {
+					relacionIE = relacionIE - 0.1;
+					if (relacionIE <= -MAX_RIE) {
+						relacionIE = -MAX_RIE;
+					}
+					if (relacionIE > 0 && relacionIE < 1) {
+						relacionIE = -1;
+					}
+					// Calculo del tiempo I:E
+					if (relacionIE > 0) {
+						inspirationTime = (60 / frecRespiratoria) / (1 + relacionIE);
+						expirationTime = relacionIE * inspirationTime;
+					}
+					else {
+						expirationTime = (60 / frecRespiratoria) / (1 - relacionIE);
+						inspirationTime = -relacionIE * expirationTime;
+					}
+				}
+				//Serial.println("I :" + String(inspirationTime));
+				//Serial.println("E :" + String(expirationTime));
+				break;
+			case 2:
+				if (flagPresion) {
+					maxPresion--;
+					if (maxPresion > MAX_PRESION) {
+						maxPresion = MAX_PRESION;
+					}
+				}
+				else if (flagFlujo) {
+					maxFlujo--;
+					if (maxFlujo > MAX_FLUJO) {
+						maxFlujo = MAX_FLUJO;
+					}
+				}
+				break;
+			}
+		}
+	}
+	// portENTER_CRITICAL_ISR(&mux);
+	flagEncoder = 3;
+	//lcd_show();
 
-  // portEXIT_CRITICAL_ISR(&mux);
+	// portEXIT_CRITICAL_ISR(&mux);
 }
+
+
+// *************************************************
+// **** Atencion a rutina de interrupcion por timer
+// *************************************************
+// Cycling of the Mechanical Ventilator
+void cycling() {
+	if (flagTimerInterrupt) {
+		flagTimerInterrupt = false;
+		interruptCounter++;
+		contADC++;
+
+		if (contADC == 50) {
+			fl_ADC = true;
+			contADC = 0;
+		}
+
+		if (interruptCounter == 1) {        // Inicia el ciclado abriendo electrovalvula de entrada y cerrando electrovalvula de salida
+			digitalWrite(EV_01_P1, HIGH);   // turn the LED on (HIGH is the voltage level)
+			digitalWrite(EV_02_P1, LOW);	// turn the LED on (HIGH is the voltage level)
+			digitalWrite(EV_03_P1, LOW);	// turn the LED on (HIGH is the voltage level)
+
+			//Calculos
+			Pin_min = SPin;
+			Pout_min = SPout;
+		}
+		else if (interruptCounter == int(inspirationTime * 1000)) { // espera 1 segundo y cierra electrovalvula de entrada y abre electrovalvula de salida
+			digitalWrite(EV_01_P1, LOW);   // turn the LED on (HIGH is the voltage level)
+			digitalWrite(EV_02_P1, HIGH);  // turn the LED off by making the voltage LOW
+			digitalWrite(EV_03_P1, HIGH);  // turn the LED off by making the voltage LOW
+
+			//Calculos
+			Pin_max = SPin;
+			Pout_max = SPout;
+			dpmax = Pin_max - Pin_min;
+			if (dpmax < 0) {
+				dpmax = -dpmax;
+			}
+			dpmin = Pout_max - Pout_min;
+			if (dpmin < 0) {
+				dpmin = -dpmin;
+			}
+		}
+		else if (interruptCounter >= int(((inspirationTime + expirationTime) * 1000))) {
+			interruptCounter = 0;
+			Peep = UmbralPeep;
+			UmbralPeep = 100;
+			Ppico = UmbralPpico - 2;
+			UmbralPpico = -100;
+			Comp = (C1 * Peep * Peep) - (C2 * Peep) + C3;
+			//Comp=(-14.52*Peep)+(198.75);
+
+			//Filtrado media movil VT y COMP
+			//-Almacenamiento de valores
+			V_Comp[9] = Comp;
+			V_dpmax[9] = dpmax;
+			V_dpmin[9] = dpmin;
+			//-Corrimiento inicial
+			for (char i = 9; i >= 1; i--) {
+				V_Comp[9 - i] = V_Comp[9 - i + 1];
+				V_dpmax[9 - i] = V_dpmax[9 - i + 1];
+				V_dpmin[9 - i] = V_dpmin[9 - i + 1];
+			}
+			//-Calculo promedio
+			SComp = 0;
+			Sdpmax = 0;
+			Sdpmin = 0;
+			for (char i = 0; i <= 9; i++) {
+				SComp = SComp + V_Comp[i];
+				Sdpmax = Sdpmax + V_dpmax[i];
+				Sdpmin = Sdpmin + V_dpmin[i];
+			}
+			SComp = SComp / 10;
+			Sdpmax = Sdpmax / 10;
+			Sdpmin = Sdpmin / 10;
+			VT = SComp * (Sdpmax - Sdpmin);
+		}
+
+		if (flagDettachInterrupt_A) {
+			if (contDetach == 0 && flagDetach == false) {
+				detachInterrupt(A);  // desactiva la interrupcion A
+				flagDetach = true;
+			}
+			contDetach++;
+			if (contDetach >= 150) {
+				attachInterrupt(digitalPinToInterrupt(A), encoderInterrupt_A, FALLING);
+				flagDettachInterrupt_A = false;
+				flagDetach = false;
+				contDetach = 0;
+				digitalWrite(14, HIGH);
+				lcd_show();
+				digitalWrite(14, LOW);
+			}
+		}
+		if (flagDettachInterrupt_B) {
+			if (contDetach == 0 && flagDetach == false) {
+				detachInterrupt(B);  // desactiva la interrupcion A
+				flagDetach = true;
+			}
+			contDetach++;
+			if (contDetach >= 150) {
+				attachInterrupt(digitalPinToInterrupt(B), encoderInterrupt_B, FALLING);
+				flagDettachInterrupt_B = false;
+				flagDetach = false;
+				contDetach = 0;
+				digitalWrite(14, HIGH);
+				lcd_show();
+				digitalWrite(14, LOW);
+			}
+		}
+		milisecond++;
+		if (milisecond == 1000) {
+			milisecond = 0;
+			second++;
+			if (second == 60) {
+				second = 0;
+			}
+		}
+	}// Final interrupcion timer
+} // end cycling()
--- ventilator/ventilator.ino
+++ ventilator/ventilator.ino
@@ -6,11 +6,8 @@
 
 // Definiciones para controlar el shiel DFRobot quad motor driver
 #define EV_01_P1        25  // equivale al pin 3 del shield, velocidad motor 1
-// #define EV_01_P2        26  // equivale al pin 4 del shield, direccion motor 1 // movido del 17 para prueba
 #define EV_02_P1        27  // equivale al pin 6 del shield, velocidad motor 4
-// #define EV_02_P2        14  // equivale al pin 7 del shield, direccion motor 4
 #define EV_03_P1        23  // equivale al pin 11 del shield, velocidad motor 2
-// #define EV_03_P2        13  // equivale al pin 12 del shield, direccion motor 2
 
 // Definiciones para el manejo del ADC
 #define ADC_PRESS_1     39  // pin ADC para presion 1
@@ -31,17 +28,16 @@
 #define OFFS2      -32.354
 
 // Calibracion de los sensores de presion
-#define AMP1       0.0308
-#define OFFS1      -22.673
-#define AMP2       0.0309
-#define OFFS2      -32.354
-#define AMP3       0.0309
-#define OFFS3      -32.354
+#define AMP1	0.0308
+#define OFFS1   -22.673
+#define AMP2    0.0309
+#define OFFS2   -32.354
+#define AMP3    0.0309
+#define OFFS3   -32.354
 
 // Variables de control del protocolo
 #define RXD2 16
 #define TXD2 17
-
 
 // variables y parametros de impresion
 String SERIALEQU;
@@ -129,7 +125,6 @@ volatile float expirationTime = 3.333;
 // definiciones del timer
 volatile int interruptCounter = 0;
 
-
 // inicializacion del contador del timer
 hw_timer_t* timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
@@ -137,13 +132,24 @@ portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 
 // definicion de interrupciones
 void IRAM_ATTR onTimer();  // funcion de interrupcion
-
 void IRAM_ATTR swInterrupt();
 void IRAM_ATTR encoderInterrupt_A();
 void IRAM_ATTR encoderInterrupt_B();
 
+// Creation of task handle
+TaskHandle_t TaskOnCore0;
+
+//void Task1code(void* pvParameters) {
+//	Serial.print("Task1 running on core ");
+//	Serial.println(xPortGetCoreID());
+//
+//	for (;;) {
+//		//cycling();
+//	}
+//}
 
 void setup() {
+
 	// Configuracion del timer a 1 kHz
 	timer = timerBegin(0, 80, true);               // Frecuencia de reloj 80 MHz, prescaler de 80, frec 1 MHz
 	timerAttachInterrupt(timer, &onTimer, true);
@@ -166,13 +172,10 @@ void setup() {
 
 	// inicializacion de los pines controladores de las EV como salidas
 	pinMode(EV_01_P1, OUTPUT);  // PIN 3   velocidad
-	//  pinMode(EV_01_P2, OUTPUT);  // PIN 4   direccion
 	pinMode(EV_02_P1, OUTPUT);  // PIN 6   velocidad
-	//  pinMode(EV_02_P2, OUTPUT);  // PIN 7   direccion
 	pinMode(EV_03_P1, OUTPUT);  // PIN 12  velocidad
-	//  pinMode(EV_03_P2, OUTPUT);  // PIN 11  direccion
 
-	Serial.begin(115200);   // incializacion de comunicacion serie a 9600 bps
+	Serial.begin(115200);
 	Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
 	lcd_setup();
 
@@ -189,152 +192,30 @@ void setup() {
 	RaspberryChain = String("");
 
 	lcd_show();
+	pinMode(14, OUTPUT);
 
+	//// Task assigned to Core 1
+	//xTaskCreatePinnedToCore(
+	//	Task1code,		/* Function to implement the task */
+	//	"Task1",		/* Name of the task */
+	//	10000,			/* Stack size in words */
+	//	NULL,			/* Task input parameter */
+	//	1,				/* Priority of the task */
+	//	&TaskOnCore0,	/* Task handle. */
+	//	0);				/* Core where the task should run */
 
-pinMode(14, OUTPUT);
-
+	//// Check if this delay is necesary
+	//delay(500);
 }
 
 void loop() {
 
-  // *************************************************
-  // **** Atencion a rutina de interrupcion por timer
-  // *************************************************
-  if (flagTimerInterrupt) {
-    flagTimerInterrupt = false;
-    interruptCounter++;
-    contADC++;
-
-    if (contADC == 50) {
-      fl_ADC = true;
-      contADC = 0;
-    }
-
-
-    if (interruptCounter == 1) {        // Inicia el ciclado abriendo electrovalvula de entrada y cerrando electrovalvula de salida
-      //  digitalWrite(EV_01_P2, HIGH);   // turn the LED on (HIGH is the voltage level)
-      digitalWrite(EV_01_P1, HIGH);   // turn the LED on (HIGH is the voltage level)
-
-      digitalWrite(EV_02_P1, LOW);   // turn the LED on (HIGH is the voltage level)
-      //  digitalWrite(EV_02_P2, LOW);   // turn the LED on (HIGH is the voltage level)
-      digitalWrite(EV_03_P1, LOW);   // turn the LED on (HIGH is the voltage level)
-      //  digitalWrite(EV_03_P2, LOW);   // turn the LED on (HIGH is the voltage level)
-      
-      //Calculos
-			Pin_min = SPin;
-			Pout_min = SPout;
-      
-    }
-    else if (interruptCounter == int(inspirationTime * 1000)) { // espera 1 segundo y cierra electrovalvula de entrada y abre electrovalvula de salida
-      //  digitalWrite(EV_01_P2, LOW);    // turn the LED off by making the voltage LOW
-      digitalWrite(EV_01_P1, LOW);   // turn the LED on (HIGH is the voltage level)
-
-      digitalWrite(EV_02_P1, HIGH);    // turn the LED off by making the voltage LOW
-      //  digitalWrite(EV_02_P2, HIGH);   // turn the LED on (HIGH is the voltage level)
-      digitalWrite(EV_03_P1, HIGH);    // turn the LED off by making the voltage LOW
-      //  digitalWrite(EV_03_P2, HIGH);   // turn the LED on (HIGH is the voltage level)
-      
-      //Calculos
-			Pin_max = SPin;
-			Pout_max = SPout;
-			dpmax = Pin_max - Pin_min;
-			if (dpmax < 0) {
-				dpmax = -dpmax;
-			}
-			dpmin = Pout_max - Pout_min;
-			if (dpmin < 0) {
-				dpmin = -dpmin;
-			}
-      
-    }
-    else if (interruptCounter >= int(((inspirationTime + expirationTime) * 1000))) {
-      interruptCounter = 0;
-      Peep = UmbralPeep;
-			UmbralPeep = 100;
-			Ppico = UmbralPpico - 2;
-			UmbralPpico = -100;
-			Comp = (C1 * Peep * Peep) - (C2 * Peep) + C3;
-			//Comp=(-14.52*Peep)+(198.75);
-
-			//Filtrado media movil VT y COMP
-			//-Almacenamiento de valores
-			V_Comp[9] = Comp;
-			V_dpmax[9] = dpmax;
-			V_dpmin[9] = dpmin;
-			//-Corrimiento inicial
-			for (char i = 9; i >= 1; i--) {
-				V_Comp[9 - i] = V_Comp[9 - i + 1];
-				V_dpmax[9 - i] = V_dpmax[9 - i + 1];
-				V_dpmin[9 - i] = V_dpmin[9 - i + 1];
-			}
-			//-Calculo promedio
-			SComp = 0;
-			Sdpmax = 0;
-			Sdpmin = 0;
-			for (char i = 0; i <= 9; i++) {
-				SComp = SComp + V_Comp[i];
-				Sdpmax = Sdpmax + V_dpmax[i];
-				Sdpmin = Sdpmin + V_dpmin[i];
-			}
-			SComp = SComp / 10;
-			Sdpmax = Sdpmax / 10;
-			Sdpmin = Sdpmin / 10;
-			VT = SComp * (Sdpmax - Sdpmin);
-	
-    }
-
-
-    if (flagDettachInterrupt_A) {
-      if (contDetach == 0 && flagDetach == false) {
-        detachInterrupt(A);  // desactiva la interrupcion A
-        flagDetach = true;
-      }
-      contDetach++;
-      if (contDetach >= 150) {
-        attachInterrupt(digitalPinToInterrupt(A), encoderInterrupt_A, FALLING);
-        flagDettachInterrupt_A = false;
-        flagDetach = false;
-        contDetach = 0;
-        digitalWrite(14, HIGH);
-        lcd_show();
-        digitalWrite(14, LOW);
-      }
-    }
-
-    if (flagDettachInterrupt_B) {
-      if (contDetach == 0 && flagDetach == false) {
-        detachInterrupt(B);  // desactiva la interrupcion A
-        flagDetach = true;
-      }
-      contDetach++;
-      if (contDetach >= 150) {
-        attachInterrupt(digitalPinToInterrupt(B), encoderInterrupt_B, FALLING);
-        flagDettachInterrupt_B = false;
-        flagDetach = false;
-        contDetach = 0;
-        digitalWrite(14, HIGH);
-        lcd_show();
-        digitalWrite(14, LOW);
-      }
-    }
-    
-    milisecond ++;
-    if (milisecond == 1000) {
-      milisecond = 0;
-      second++;
-      if (second == 60) {
-        second = 0;
-      }
-    }
-
-  }
-  // Final interrupcion timer
-
-  // *************************************************
-  // **** Atencion a rutina de adquisicion ADC
-  // *************************************************
-  if (fl_ADC) {
-    fl_ADC = false;
+	cycling();
+	// *************************************************
+	// **** Atencion a rutina de adquisicion ADC
+	// *************************************************
+	if (fl_ADC) {
+		fl_ADC = false;
 		ADC1_Value = analogRead(ADC_PRESS_1);
 		ADC2_Value = analogRead(ADC_PRESS_2);
 		ADC3_Value = analogRead(ADC_PRESS_3);
@@ -344,7 +225,7 @@ void loop() {
 		Pressure3 = AMP3 * float(ADC3_Value) + OFFS3;
 
 		//Filtrado media movil
-	//-Almacenamiento de valores
+		//-Almacenamiento de valores
 		Pin[9] = Pressure1;
 		Pout[9] = Pressure2;
 		//-Corrimiento inicial
@@ -402,42 +283,39 @@ void loop() {
 		  Serial.print(ADC2_Value + 180);
 		  Serial.print(", Puls = ");
 		  Serial.println(numeroPulsos);*/
-  }
-  // Final muestreo ADC
+	}// Final muestreo ADC
 
 
-  // *************************************************
-  // **** Atencion a rutina de interrupcion por switch
-  // *************************************************
-  if (flagSwInterrupt) {
-    flagSwInterrupt = false;
-    // Atencion a la interrupcion
-    swInterruptAttention();
-  }
-  // Final interrupcion Switch
+	// *************************************************
+	// **** Atencion a rutina de interrupcion por switch
+	// *************************************************
+	if (flagSwInterrupt) {
+		flagSwInterrupt = false;
+		// Atencion a la interrupcion
+		swInterruptAttention();
+	}
+	// Final interrupcion Switch
 
 
-  // *************************************************
-  // **** Atencion a rutina de interrupcion por encoder
-  // *************************************************
-  if (flagEncoderInterrupt_A) {
-    flagEncoderInterrupt_A = false;
-    // Atencion a la interrupcion
-    encoderInterruptAttention_A();
-  }
-  // Final interrupcion encoder
+	// *************************************************
+	// **** Atencion a rutina de interrupcion por encoder
+	// *************************************************
+	if (flagEncoderInterrupt_A) {
+		flagEncoderInterrupt_A = false;
+		// Atencion a la interrupcion
+		encoderInterruptAttention_A();
+	}
+	// Final interrupcion encoder
 
 
-  // *************************************************
-  // **** Atencion a rutina de interrupcion por encoder
-  // *************************************************
-  if (flagEncoderInterrupt_B) {
-    flagEncoderInterrupt_B = false;
-    // Atencion a la interrupcion
-    encoderInterruptAttention_B();
-  }
-  // Final interrupcion encoder
-	         
+	// *************************************************
+	// **** Atencion a rutina de interrupcion por encoder
+	// *************************************************
+	if (flagEncoderInterrupt_B) {
+		flagEncoderInterrupt_B = false;
+		// Atencion a la interrupcion
+		encoderInterruptAttention_B();
+	}// Final interrupcion encoder
 }
 
 void IRAM_ATTR onTimer() {
